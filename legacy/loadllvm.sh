e95e88fe1526ecc137d6743e1d9413c89274e06c
Anton Rydahl <rydahl1@tioga10.llnl.gov>
2023-05-26 15:02:28 -0700

    Restructured project

---
+++ legacy/loadllvm.sh
@@ -0,0 +1,8 @@
+module load gcc
+export PATH=/g/g92/rydahl1//LLVM/install/bin:$PATH
+export LD_LIBRARY_PATH=/g/g92/rydahl1/LLVM/install/include:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/g/g92/rydahl1/LLVM/install/lib:$LD_LIBRARY_PATH
+export FC=flang-new
+export CC=clang
+export gFC=gfortran
+export gCC=gcc
--- loadllvm.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/Users/rydahl1/Documents/LLVM/bin:$PATH
-export LD_LIBRARY_PATH=/Users/rydahl1/Documents/LLVM/include:$LD_LIBRARY_PATH
-export FC=flang-new
-export CC=clang
