b84522e789bf0b3010257366fc3455d15bd64729
Anton Rydahl <rydahl1@tioga25.llnl.gov>
2023-05-31 16:45:46 -0700

    Added test that is not yet supported in flang-new

--- cbindings_offloading/caller.c
+++ cbindings_offloading/caller.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#define TEST_ARR_LEN 10
+#include <stdlib.h>
+#define TEST_ARR_LEN 100000000
 
 #pragma omp declare target
 void increment_at(int i, int *array);
 int increment(int i);
+void increment_loop(int * len,int *array);
 #pragma omp end declare target
 
 void increment_subroutine(int *b, int n) {
 #pragma omp target map(tofrom : b [0:n])
+#pragma omp teams distribute parallel for
   for (int i = 0; i < n; i++) {
     increment_at(i, b);
   }
@@ -15,25 +18,42 @@ void increment_subroutine(int *b, int n) {
 
 void increment_function(int *b, int n) {
 #pragma omp target map(tofrom : b [0:n])
+#pragma omp teams distribute parallel for
   for (int i = 0; i < n; i++) {
     b[i] = increment(b[i]);
   }
 }
 
+void increment_impl_loop(int * b, int n) {
+#pragma omp target map(tofrom : b [0:n])
+  increment_loop(&n,b);
+}
+
 int main() {
-  int arr[TEST_ARR_LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  
+  int * arr = malloc(TEST_ARR_LEN*sizeof(int));
+
+  #pragma omp parallel for
+  for (int i = 0; i < TEST_ARR_LEN; i++) arr[i] = i;
+ 
   printf("Calling subroutine from the device\n");
   increment_subroutine(arr, TEST_ARR_LEN);
-  for (int i = 0; i < TEST_ARR_LEN; i++) {
+  for (int i = 0; i < 10; i++) {
     printf("%d = %d\n", i, arr[i]);
   }
-  printf("Calling function from the device\n");
 
+  printf("Calling function from the device\n");
   increment_function(arr, TEST_ARR_LEN);
-  for (int i = 0; i < TEST_ARR_LEN; i++) {
+  for (int i = 0; i < 10; i++) {
     printf("%d = %d\n", i, arr[i]);
   }
+
+  printf("Calling function with implicit loop from the device\n");
+  increment_impl_loop(arr, TEST_ARR_LEN);
+  for (int i = 0; i < 10; i++) {
+    printf("%d = %d\n", i, arr[i]);
+  }
+
+  free(arr);
 
   return 0;
 }
--- cbindings_offloading/offload.f90
+++ cbindings_offloading/offload.f90
@@ -8,7 +8,26 @@ end subroutine
 
 function a_function(input) result(output) bind(C, name="increment")
     use ISO_C_BINDING
-    integer (C_INT), intent(in), VALUE :: input
-    integer (C_INT)		:: output
+    integer (C_INT), intent(in), VALUE 	:: input
+    integer (C_INT)  			:: output
     output = input + 1
 end function
+
+subroutine implicit_loop_subroutine(length, arr) bind(C, name="increment_loop")
+    use ISO_C_BINDING
+    integer (C_INT), intent(in)				:: length
+    integer (C_INT), dimension(*), intent(inout) 	:: arr
+    arr(1:length) = arr(1:length) + 1
+end subroutine
+
+subroutine parallel_do_increment(length, arr) bind(C, name="increment_parallel_for")
+    use ISO_C_BINDING
+    integer (C_INT), intent(in)                         :: length
+    integer (C_INT), dimension(*), intent(inout)        :: arr
+    integer (C_INT)					:: i
+! omp parallel do implicit(none) private(i) shared(arr,length)
+    do i=1,length
+        arr(i) = arr(i) + 1
+    end do
+! omp end parallel do
+end subroutine
